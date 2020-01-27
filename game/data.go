6e0a6a91c5457d0a82a8af0a1abde0023960f96f
kplat1 <koreilly5297@gmail.com>
2020-01-26 20:02:42 -0800

    Fixed bug

--- game/data.go
+++ game/data.go
@@ -6,16 +6,27 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
+"io/ioutil"
+"os"
 	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
 func data() {
-	var err error
-
-	db, err = sql.Open("postgres", "postgres://mesnfvhztxvwes:abd64ff99d5342f8f88f93248bba2d1c34821cc228dd847ca119287417516604@ec2-107-20-185-27.compute-1.amazonaws.com:5432/da7ng88d7bth78")
+	var str string
+	var b []byte
+	home, err := os.UserHomeDir()
+	fn := fmt.Sprintf("%v/dburl/url.txt", home)
+	b, err = ioutil.ReadFile(fn)
+	    if err != nil {
+				// fmt.Printf("%v \n", err)
+	        str = "example.com"
+	    } else {
+	    str = string(b) // convert content to a 'string'
+		}
+			// fmt.Printf("Test String: %v \n", str)
+	db, err = sql.Open("postgres", str)
 	if err != nil {
 		panic(err)
 	}
