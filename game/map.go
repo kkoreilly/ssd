28b2639580fa52b488fd799262aeb95850b958ec
kplat1 <koreilly5297@gmail.com>
2020-03-05 11:42:15 -0800

    Updated map

--- game/map.go
+++ game/map.go
@@ -21,7 +21,8 @@ var FirstMap = Map{
 	// "House1":    {"House", mat32.Vec3{10, 0, -40}, DefScale},
 	// "House1": {"House", mat32.Vec3{0, 0, -10}, DefScale},
 	// "House2": {"House", mat32.Vec3{20, 0, -10}, DefScale},
-	"Block1":   {"Block", mat32.Vec3{-80, 0, -10}, DefScale},
+	"House1":   {"House", mat32.Vec3{0, 0, -20}, DefScale},
+	"House2":   {"House", mat32.Vec3{0, 0, 20}, DefScale},
 	"TheWall1": {"TheWall", mat32.Vec3{0, 0, 0}, DefScale},
 }
 
